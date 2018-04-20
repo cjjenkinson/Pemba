6992dbdb102f2dd176efa031ec2413e2eb330065
Cameron Jenkinson <camjenkinson@gmail.com>
2018-04-20 08:39:57 +0200

    Add render elements to DOM

---
+++ src/reconciler.js
@@ -0,0 +1,39 @@
+/**
+ * render an element and its children to the DOM.
+ * @param {object} element - Element to render.
+ * @param {object} parentDom - Root DOM node.
+ */
+
+export function render(element, parentDom) {
+	const { type, props } = element;
+
+	// Create the DOM element
+	const isTextElement = type == 'TEXT ELEMENT';
+	const dom = isTextElement ? document.createTextNode('') : document.createElement(type);
+
+	// Add event listeners
+	const isListener = name => name.startsWith('on');
+
+	Object.keys(props)
+		.filter(isListener)
+		.forEach(name => {
+			const eventType = name.toLowerCase.substring(2);
+			dom.addEventListener(eventType, props[name]);
+		});
+
+	// Set properties
+	const isAttribute = name => !isListener(name) && name != 'children';
+
+	Object.keys(props)
+		.filter(isAttribute)
+		.forEach(name => {
+			dom[name] = props[name];
+		});
+
+	// Render children
+	const childElements = props.children || [];
+	childElements.forEach(childElement => render(childElement, dom));
+
+	// Append to parent
+	parentDom.appendChild(dom);
+}
